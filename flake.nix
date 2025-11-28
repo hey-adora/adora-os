7cdbd72e7ae7d0a56a801a0734ed43ebc26acc10
hey <146812294+hey-adora@users.noreply.github.com>
2025-11-28 13:59:07 +0200

    fix disko

--- flake.nix
+++ flake.nix
@@ -49,45 +49,7 @@
       nixosConfigurations.adora = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         specialArgs = { inherit inputs; };
-        disko.devices = {
-          disk = {
-            main = {
-              # When using disko-install, we will overwrite this value from the commandline
-              device = "/dev/disk/by-id/some-disk-id";
-              type = "disk";
-              content = {
-                type = "gpt";
-                partitions = {
-                  MBR = {
-                    type = "EF02"; # for grub MBR
-                    size = "1M";
-                    priority = 1; # Needs to be first partition
-                  };
-                  ESP = {
-                    type = "EF00";
-                    size = "500M";
-                    content = {
-                      type = "filesystem";
-                      format = "vfat";
-                      mountpoint = "/boot";
-                      mountOptions = [ "umask=0077" ];
-                    };
-                  };
-                  root = {
-                    size = "100%";
-                    content = {
-                      type = "filesystem";
-                      format = "ext4";
-                      mountpoint = "/";
-                    };
-                  };
-                };
-              };
-            };
-          };
-        };
         modules = [
-          disko.nixosModules.disko
           ({ config, lib, pkgs, ... }:
 
             {
@@ -142,6 +104,46 @@
               system.stateVersion = "25.11";
 
             })
+                      disko.nixosModules.disko
+            (
+            disko.devices = {
+          disk = {
+            main = {
+              # When using disko-install, we will overwrite this value from the commandline
+              device = "/dev/disk/by-id/some-disk-id";
+              type = "disk";
+              content = {
+                type = "gpt";
+                partitions = {
+                  MBR = {
+                    type = "EF02"; # for grub MBR
+                    size = "1M";
+                    priority = 1; # Needs to be first partition
+                  };
+                  ESP = {
+                    type = "EF00";
+                    size = "500M";
+                    content = {
+                      type = "filesystem";
+                      format = "vfat";
+                      mountpoint = "/boot";
+                      mountOptions = [ "umask=0077" ];
+                    };
+                  };
+                  root = {
+                    size = "100%";
+                    content = {
+                      type = "filesystem";
+                      format = "ext4";
+                      mountpoint = "/";
+                    };
+                  };
+                };
+              };
+            };
+          };
+        };
+            )
         ];
       };
 
