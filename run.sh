675041e5e28f78e38a4bc5e08157f7c910f714f4
hey <146812294+hey-adora@users.noreply.github.com>
2026-01-26 15:44:08 +0200

    [fix] run script name && created flake.lock

--- run.sh
+++ run.sh
@@ -5,5 +5,5 @@
 rm -f hardware-configuration.nix
 nixos-generate-config --show-hardware-config --no-filesystems > hardware-configuration.nix &&\
 git add . &&\
-sudo nix run --extra-experimental-features "nix-command flakes" "github:nix-community/disko/latest#disko-install" -- --flake ".#adora" --disk main /dev/vda
+sudo nix run --extra-experimental-features "nix-command flakes" "github:nix-community/disko/latest#disko-install" -- --flake ".#main" --disk main /dev/vda
 
