95e0e6f868b3b030c71b47283f84c2a72622d3c8
hey <146812294+hey-adora@users.noreply.github.com>
2026-08-24 13:04:40 +0300

    v5

---
+++ v5/run.sh
@@ -0,0 +1,5 @@
+rm -f hardware-configuration.nix
+nixos-generate-config --show-hardware-config --no-filesystems > hardware-configuration.nix &&\
+git add.
+sudo nix run --extra-experimental-features "nix-command flakes" "github:nix-community/disko/latest#disko-install" -- --flake ".#b650" --disk main /dev/vda
+
