32c6bd89c26463ec6b93b4e0bb19e9c62a6196de
hey <146812294+hey-adora@users.noreply.github.com>
2026-09-02 15:31:13 +0300

    Delete run.sh

--- run.sh
+++
@@ -1,5 +0,0 @@
-rm -f hardware-configuration.nix
-nixos-generate-config --show-hardware-config --no-filesystems > hardware-configuration.nix &&\
-git add .
-sudo nix run --extra-experimental-features "nix-command flakes" "github:nix-community/disko/latest#disko-install" -- --flake ".#b650" --disk main /dev/nvme0n1
-
